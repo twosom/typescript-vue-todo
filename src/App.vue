0ac504bec5b9cf8173278b7af1aff5dc64ca1dea
hope <hope@gravylab.co.kr>
2022-04-17 19:29:43 +0900

    todo 조회, 추가, 삭제 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <header>
+      <h1>Vue Todo with TypeScript</h1>
+    </header>
+    <main>
+      <todo-input :item="todoText" @input="updateTodoText" @add="addTodoItem" />
+    </main>
+    <div>
+      <ul>
+        <todo-list-item
+          v-for="(todoItem, index) in todoItems"
+          :key="index"
+          :index="index"
+          :todo-item="todoItem"
+          @remove="removeTodoItem"
+        />
+      </ul>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoInput from "@/components/TodoInput.vue";
+import TodoListItem from "@/components/TodoListItem.vue";
+
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  save(todoItems: Array<any>) {
+    const parsed: string = JSON.stringify(todoItems);
+    localStorage.setItem(STORAGE_KEY, parsed);
+  },
+  fetch(): any[] {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
+    return JSON.parse(todoItems);
+  },
+};
 
 export default Vue.extend({
-  name: "App",
   components: {
-    HelloWorld,
+    TodoInput,
+    TodoListItem,
+  },
+  data() {
+    return {
+      todoText: "",
+      todoItems: [] as any[],
+    };
+  },
+
+  methods: {
+    updateTodoText(value: string): void {
+      this.todoText = value;
+    },
+
+    addTodoItem(): void {
+      const value = this.todoText;
+      this.todoItems.push(value);
+      storage.save(this.todoItems);
+      this.initTodoText();
+    },
+
+    initTodoText(): void {
+      this.todoText = "";
+    },
+
+    fetchTodoItems(): any[] {
+      return storage.fetch();
+    },
+
+    removeTodoItem(index: number) {
+      console.log("remove", index);
+      this.todoItems.splice(index, 1);
+      storage.save(this.todoItems);
+    },
+  },
+  created() {
+    this.todoItems = this.fetchTodoItems();
   },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <label for="todo-input">오늘 할 일 : </label>
+    <input id="todo-input" type="text" :value="item" @input="handleInput" />
+    <button type="button" @click="addTodo">추가</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    item: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    addTodo(): void {
+      this.$emit("add");
+    },
+    handleInput(event: InputEvent) {
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit("input", eventTarget.value);
+    },
+  },
+});
+</script>
+
+<style scoped></style>
